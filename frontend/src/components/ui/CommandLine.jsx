8568f605d7a7fccc086ae9db0810fad957cb0555
Taylor Albright <jtayloralbright@gmail.com>
2025-03-01 22:59:06 -0600

    Cleaned up commandObject. CommandLine and handleEnterPress are much cleaner

--- frontend/src/components/ui/CommandLine.jsx
+++ frontend/src/components/ui/CommandLine.jsx
@@ -6,23 +6,15 @@ import { zGameContext } from "./zGameContext"
 import { WebSocketContext } from "./WebSocketContext"
 
 export const CommandLine = () => {
-    const { player, setPlayer, addLog, currentArea, npcs, setNpcs, enemies, 
-    setEnemies, setCurrentArea, items, setItems, playerItems, setPlayerItems,
-    players, setPlayers, playerStatus
-    } = useContext(zGameContext)
     const { socket, sendMessage } = useContext(WebSocketContext)
     const commandObject = useContext(zGameContext)
     commandObject.socket = socket
     commandObject.sendMessage = sendMessage
-
     return (
         <Input 
             type="text"
             style={{border: "4px solid green", height: "30px", padding: 0}}
-            onKeyDown={(e) => {handleEnterPress(e, player, setPlayer, addLog, currentArea, npcs, 
-            enemies, setCurrentArea, items, setItems, playerItems, setPlayerItems, sendMessage, socket,
-            players, setPlayers, setNpcs, setEnemies, playerStatus
-            )}}
-            />
+            onKeyDown={(e) => {handleEnterPress(e, commandObject)}}
+        />
     )
 }
--- frontend/src/managers/_handlePlayerActions.js
+++ frontend/src/managers/_handlePlayerActions.js
@@ -4,57 +4,29 @@ import { moveDirection } from "./playerMovement"
 import { command1Alias } from "./command1Alias"
 import { zGameContext } from "../components/ui/zGameContext"
 import { actionList } from "./actionList"
+import { normalizeInput } from "./normalizeInput"
 
 
-export const handleEnterPress = (e, player, setPlayer, addLog, 
-    currentArea, npcs, enemies, setCurrentArea, items, setItems, 
-    playerItems, setPlayerItems, sendMessage, socket, players,
-    setPlayers, setNpcs, setEnemies, playerStatus
-    ) => {
+export const handleEnterPress = (e, commandObject) => {
     //You can just use sendMessage to use the shorthand version. I'm using socket.send to
     //  get practice with typing it out
     if (e.keyCode != 13) {return}
-    let rawInput = e.target.value
+    const rawInput = e.target.value
+    const { player, socket } = commandObject
     if (rawInput[0] === "'") {
         let normalizedInput = rawInput.slice(1)
-        // sendMessage(rawInput)
         socket.send(JSON.stringify({type: "playerDialogue", playerId: player.id, areaId: player.area_id, playerName: player.name, playerDialogue: normalizedInput}))
-        // rawInput = `${player.name} says, "${rawInput.slice(1)}"`
-        // addLog(rawInput)
         e.target.value = ""
         return
     }
-    const userInput = normalizeInput(rawInput)
-    const commandObject = {
-        command1: command1Alias(userInput[0]),
-        command2: userInput[1],
-        command3: userInput[2],
-        command4: userInput[3],
-        player: player,
-        setPlayer: setPlayer,
-        addLog: addLog,
-        currentArea: currentArea,
-        npcs: npcs,
-        enemies: enemies,
-        setCurrentArea: setCurrentArea,
-        items: items,
-        setItems: setItems,
-        playerItems: playerItems,
-        setPlayerItems: setPlayerItems,
-        players: players,
-        setPlayers: setPlayers,
-        setNpcs: setNpcs,
-        setEnemies: setEnemies,
-        socket: socket,
-        playerStatus: playerStatus
-    }
-    actionList(commandObject)
     e.target.value = ""
+    const userInput = normalizeInput(rawInput)
+    commandObject.command1 = command1Alias(userInput[0])
+    commandObject.command2 = userInput[1]
+    commandObject.command3 = userInput[2]
+    commandObject.command4 = userInput[3]
+    actionList(commandObject)
 }
 
-export const normalizeInput = (input) => {
-    const trimmedInput = input.toLowerCase().trim().split(" ").filter(element => element.length > 0)
-    return trimmedInput
-}
 
 
